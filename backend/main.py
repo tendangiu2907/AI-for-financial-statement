01ad8bf6cd8890b4f0f88fbac34a868c1a383471
huengoc <imngochue0060@gmail.com>
2025-04-04 09:29:56 +0700

    feat: dockerize backend frontend

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from api.v1.detect_table_router import router as detect_router
 import os
-from core.config import EXTRACTED_FOLDER, UPLOAD_DIR, SERVER_ADDRESS, SERVER_PORT
+from core.config import EXTRACTED_FOLDER, UPLOAD_DIR
 
 # Đảm bảo các thư mục tồn tại
 os.makedirs(EXTRACTED_FOLDER, exist_ok=True)
@@ -15,7 +15,3 @@ app = FastAPI()
 app.include_router(detect_router, prefix="/api/v1")
 
 app.mount(f"/{EXTRACTED_FOLDER}", StaticFiles(directory=EXTRACTED_FOLDER), name=EXTRACTED_FOLDER)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host=SERVER_ADDRESS, port=SERVER_PORT)
